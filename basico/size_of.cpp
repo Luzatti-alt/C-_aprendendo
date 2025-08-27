22a6ae4a4127fe91297baa7db1d89693f6b395c8
luzatti-alt <lucasguzatti4@gmail.com>
2025-08-27 19:27:55 -0300

    size_of

--- basico/size_of.cpp
+++ basico/size_of.cpp
@@ -4,5 +4,9 @@ int main(){
     double nota = 2.5;
     std::cout<<sizeof(nota)<<" Bytes \n";//tamanho maximo alocado na variavel
     //comeco de manipulação de memoria apartir daqui
+    //o valor varia conforme dados e tipo usado
+    //outro exemplo usando os bytes do array e d std::string podemos achar a qnt de elementos
+    std::string alunos[]={"fulano","sigma","beta","gamma"};
+    std::cout<<sizeof(alunos)/sizeof(std::string)<<" elementos no array";
     return 0;
 }
