32497ec54f69375a8d30286c4332ff376df4f519
luzatti-alt <lucasguzatti4@gmail.com>
2025-08-27 19:22:59 -0300

    começando algo interessante

---
+++ basico/size_of.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main(){
+    //sizeof define o tamanho de BYTES de variaveis,tipos de dados, classes,objs etc
+    double nota = 2.5;
+    std::cout<<sizeof(nota)<<" Bytes \n";//tamanho maximo alocado na variavel
+    //comeco de manipulação de memoria apartir daqui
+    return 0;
+}
