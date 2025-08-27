2435f1b300dcc8f214834887c4cd4c32eefad25e
luzatti-alt <lucasguzatti4@gmail.com>
2025-08-27 14:33:15 -0300

    algumas coisas aprendidas

---
+++ basico/input.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main(){
+    std::string a;
+    std::cout<<"digite seu nome:"<<std::endl;
+    std::cin>>a;
+    std::cout<<a<<" foi digitado"<<std::endl;
+    //se for no vscode tem que configurar para o input funcionar
+    //code runner run in terminal
+    return 0;
+}
