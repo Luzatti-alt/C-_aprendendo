9da0372c8413e8b72ba930a3cdec041872131067
luzatti-alt <lucasguzatti4@gmail.com>
2025-08-27 14:17:48 -0300

    vi mais algumas coisas

--- basico/const.cpp
+++ basico/const.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 int main(){
     //valor imutavel pi(so ira mudar se eu mudar ele nesta linha)
-    //em outras linahs ele nn ira mudar 
+    //em outras linhas ele nn ira mudar 
     const double pi = 3.1415;
     double r = 3.5;
     double calc_circunferencia = 2 *pi *r;
